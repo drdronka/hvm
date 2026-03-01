1fb218f71f06f92dcf5c203c3824c0642b6091f1
dronka <dronka@runbox.com>
2026-03-01 21:05:25 +0100

    keys added

--- src/hvm.c
+++ src/hvm.c
@@ -13,9 +13,6 @@ SDL_Renderer *renderer;
 SDL_Surface *char_surf;
 SDL_Texture *char_tex;
 
-int charx = 400;
-int chary = 300;
-
 void deinit()
 {
   if(char_surf) SDL_DestroySurface(char_surf);
@@ -31,47 +28,54 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
 
   if(!SDL_Init(SDL_INIT_VIDEO))
   {
-    SDL_Log("failed to initiaize video: %s", SDL_GetError());
+    SDL_Log("Error: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
 
   if(!SDL_CreateWindowAndRenderer(APPNAME, WINX, WINY, 0, &window, &renderer))
   {
-    SDL_Log("SDL error: %s", SDL_GetError());
+    SDL_Log("Error: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
   }
 
   char_surf = SDL_LoadPNG("./img/black_square.png");
   if(!char_surf)
   {
-    SDL_Log("failed to load square: %s", SDL_GetError());
+    SDL_Log("Error: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
 
   char_tex = SDL_CreateTextureFromSurface(renderer, char_surf);
   if(!char_tex)
   {
-    SDL_Log("SDL error: %s", SDL_GetError());
+    SDL_Log("Error: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
   }
 
   return SDL_APP_CONTINUE;
 }
 
+#define CHAR_SIZE 32
+//float xpos = (WINX / 2) - (CHAR_SIZE / 2), ypos = (WINY / 2) - (CHAR_SIZE / 2);
 float xpos = 0, ypos = 0;
-float xmax = WINX - 32, ymax = WINY-32;
+#if 0
+float xmax = WINX - 32, ymax = WINY - 32;
 uint8_t xdir = 0, ydir = 0;
-//uint16_t cntr = 0;
+#endif
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
+#if 0
   if(xdir == 0) { xpos++; } else { xpos--; };
   if(ydir == 0) { ypos++; } else { ypos--; };
   if(xpos == 0) xdir = 0;
   if(xpos == xmax) xdir = 1;
   if(ypos == 0) ydir = 0;
   if(ypos == ymax) ydir = 1;
+#endif
 
   SDL_SetRenderDrawColor(renderer, 0, 200, 200, 0);
   SDL_RenderFillRect(renderer, NULL);
-  SDL_FRect rect = {xpos, ypos, 32, 32};
+  SDL_FRect rect = {xpos, ypos, CHAR_SIZE, CHAR_SIZE};
   SDL_RenderTexture(renderer, char_tex, NULL, &rect);
 
   SDL_RenderPresent(renderer);
@@ -81,10 +85,22 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
-  if(event->type == SDL_EVENT_QUIT)
+  switch(event->type)
   {
-    deinit();
-    SDL_Quit();
+    case SDL_EVENT_KEY_DOWN:
+      const bool *keys = SDL_GetKeyboardState(NULL);
+      //switch(event->key.key)
+      //{
+        if(keys[SDL_SCANCODE_UP])    if(ypos >= CHAR_SIZE) ypos -= CHAR_SIZE;
+        if(keys[SDL_SCANCODE_DOWN])  if(ypos < WINY - CHAR_SIZE) ypos += CHAR_SIZE;
+        if(keys[SDL_SCANCODE_LEFT])  if(xpos >= CHAR_SIZE) xpos -= CHAR_SIZE;
+        if(keys[SDL_SCANCODE_RIGHT]) if(xpos < WINX - CHAR_SIZE) xpos += CHAR_SIZE;
+      //}
+      break;
+    case SDL_EVENT_QUIT:
+      deinit();
+      SDL_Quit();
+      break;
   }
 
   return SDL_APP_CONTINUE;
