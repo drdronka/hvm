91ac7ba1190419f46f1cc449e458e0306709bd16
drdronka <dronka@runbox.com>
2026-03-10 04:33:29 +0100

    corrections

--- inc/common/util.h
+++ inc/common/util.h
@@ -16,6 +16,6 @@ typedef enum
 #define ABS_DIST(x0, y0, x1, y1) SDL_sqrt((ABS(x0, x1) * ABS(x0, x1)) + (ABS(y0, y1) * ABS(y0, y1)))
 #define IS_BETWEEN(x, a, b) ((a) < (b) ? ((x) > (a) && (x) < (b)) : ((x) > (b) && (x) < (a)))
 
-#define FRAND(range) ((float)SDL_rand((range) * 1000) / 1000)
+#define FRAND(range) ((float)SDL_rand(((Sint32)(range) * (Sint32)1000)) / 1000)
 
 #endif // __GUTIL_H__
